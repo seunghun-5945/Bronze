20ca94d55b48409414d4ace3401c75acc774bd54
tmdgns59451 <tmdgns5945@oasis.inje.ac.kr>
2023-06-28 17:28:55 +0900

    BAEKJOON 2438

---
+++ 2438.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int cnt;
+	cin >> cnt;
+	for (int i = 0; i < cnt; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
